e61f5b91f0117452e2b4d3c87e2a1b8351d21327
devartqa <devartqa@gmail.com>
2019-03-21 09:32:14 +0200

    update

--- Tables/takeda.Accounts.sql
+++ Tables/takeda.Accounts.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [takeda].[Accounts] (
   [RSID1] [int] NOT NULL,
   [PQID] [int] NULL,
+  [c1] [varchar](50) NULL,
   CONSTRAINT [PK_Accounts_RSID1] PRIMARY KEY CLUSTERED ([RSID1])
 )
 ON [PRIMARY]
